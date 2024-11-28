e02de689815fe7cdd74d5fab59164229f1bc0b85
BielNova <gan2@cesar.school>
2024-11-27 23:58:48 -0300

    Index file, about me file, jpg file, script.js, styles.css

---
+++ portifolio/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio Loaded Successfully!");
+
+    // Efeito para animação de scroll
+    const hero = document.querySelector('.hero');
+    hero.classList.add('fade-in');
+
+    window.addEventListener('scroll', () => {
+        const content = document.querySelectorAll('.content, .code-snippet');
+        content.forEach((element) => {
+            const position = element.getBoundingClientRect().top;
+            const screenPosition = window.innerHeight / 1.3;
+
+            if (position < screenPosition) {
+                element.classList.add('visible');
+            }
+        });
+    });
+});
